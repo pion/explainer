2050566f3c16c07452b9ccb025d30e8feafba187
Pion <59523206+pionbot@users.noreply.github.com>
2022-04-24 02:57:00 +0000

    Update CI configs to v0.7.2
    Update lint scripts and CI configs.

--- internal/result/result.go
+++ internal/result/result.go
@@ -10,8 +10,7 @@ import (
 // SessionDetails is the combination of the Offer/Answer and what the actual state
 // of the WebRTC session is.
 //go:generate json-ice --type=SessionDetails
-type SessionDetails struct {
-}
+type SessionDetails struct{}
 
 // MarshalJSON returns the JSON encoding of this object
 func (s *SessionDetails) MarshalJSON() ([]byte, error) {
